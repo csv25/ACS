6bc94a8e93834e06b36247dd96bec853189e6bf0
Steveen Vargas <108188572+csv25@users.noreply.github.com>
2025-02-05 16:38:56 -0500

    Adding files to github

---
+++ PreProcessing_DataMining.R
@@ -0,0 +1,24 @@
+# R program to read a csv file
+
+# Get content into a data frame
+df <- read.csv("project_data.csv", header=TRUE, sep = ",")
+
+# Printing content of Text File
+#head(df, 5)
+
+# find location of missing values
+print("Position of missing values ")
+#which(is.na(df))
+
+# count total missing values 
+print("Count of total missing values  ")
+sum(is.na(df))
+
+#Sum of the missing values in each column
+sort(colSums(is.na(df)), decreasing=TRUE)
+
+#Learning the summary of some of the important variables
+""" 
+# RT is only P
+
+"""
